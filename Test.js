c564429046aa63a3d8c48dd79e765d62a98ccca3
Nischal Tamang <107337213+tmgnisc@users.noreply.github.com>
2024-02-01 12:17:21 +0545

    update react

---
+++ Test.js
@@ -0,0 +1,13 @@
+export function Test(){
+    return(
+        <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    )
+
+}
+
+
